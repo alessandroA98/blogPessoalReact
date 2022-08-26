170873f3ffbf6a7812871d086bc45dce6e77919e
alessandro <alessandro.assuncao98@gmail.com>
2022-08-26 09:52:10 -0300

    adicionando primeiro exercicio react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import'./Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1>Home</h1>
+            <p className='lorem'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, vero dolorum! Non et itaque suscipit saepe nostrum recusandae quos laborum, tenetur magnam ut fuga accusantium fugiat dolor odio ipsam laudantium?</p>
+            <br />
+            <p className='lorem'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, vero dolorum! Non et itaque suscipit saepe nostrum recusandae quos laborum, tenetur magnam ut fuga accusantium fugiat dolor odio ipsam laudantium?</p>
+        </>
+    )
+}
+
+export default Home;
+
