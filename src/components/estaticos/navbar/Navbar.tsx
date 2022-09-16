18d16ae985640f04810dea52d03aa769189f6e8d
alessandro <alessandro.assuncao98@gmail.com>
2022-09-16 13:21:12 -0300

    adicionando nav

---
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { Box } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokensReducer';
+import { addToken } from '../../../store/tokens/actions';
+import { toast } from 'react-toastify';
+import './Navbar.css'
+
+function Navbar() {
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+    );
+    let navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    function goLogout() {
+        dispatch(addToken(''));
+        toast.info('Usuário deslogado', {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: false,
+            theme: 'colored',
+            progress: undefined,
+        });
+        navigate('/login')
+    }
+
+    var navbarComponent;
+
+    if (token !== "") {
+        navbarComponent = <AppBar position="static" className='bg-menu'>
+            <Toolbar className='container'>
+                <Link to="/home" className="text-decorator-none">
+                    <Box className='cursor' >
+                        <Typography className='title' variant="h5">
+                            BlogPessoal
+                        </Typography>
+                    </Box>
+                </Link>
+
+                <Box display="flex" justifyContent="start">
+
+                    <Link to="/posts" className="text-decorator-none">
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" >
+                                postagens
+                            </Typography>
+                        </Box>
+                    </Link>
+                    <Link to="/temas" className="text-decorator-none">
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" >
+                                temas
+                            </Typography>
+                        </Box>
+                    </Link>
+                    <Link to="formularioTema" className="text-decorator-none">
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" >
+                                cadastrar tema
+                            </Typography>
+                        </Box>
+                    </Link>
+
+                    <Box mx={1} className='cursor' onClick={goLogout}>
+                        <Typography variant="h6" >
+                            logout
+                        </Typography>
+                    </Box>
+
+
+                </Box>
+
+            </Toolbar>
+        </AppBar>
+    }
+    return (
+        <>
+            {navbarComponent}
+        </>
+    )
+}
+
+export default Navbar;
